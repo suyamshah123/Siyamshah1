d7784c620daf2175a1fb95952fc4c35f959bc285
suyamshah123 <siyamonfire012@gmail.com>
2025-08-14 23:16:59 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -57,7 +57,7 @@ module.exports = {
       const totalCommands = commands.size;
       msg += `\n\n⭔Bot has ${totalCommands} commands\n⭔Type ${prefix}𝐡𝐞𝐥𝐩 <𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚗𝚊𝚖𝚎> to learn Usage.\n`;
       msg += ``;
-      msg += `\n╭─✦ADMIN: MahMUD彡\n├‣ FACEBOOK\n╰‣:m.me/mahmud.x07`; // customize this section if needed
+      msg += `\n╭─✦ADMIN: SI Y AM \n├‣ FACEBOOK\n╰‣:https://m.me/shahariya.ahmmed.siyam.01`; // customize this section if needed
 
       try {
         const hh = await message.reply({ body: msg });
